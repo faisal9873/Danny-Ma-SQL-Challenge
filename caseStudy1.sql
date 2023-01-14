a42a7d782ed3c52381d6051d66e57dbe995c10a1
faisal9873 <83473510+faisal9873@users.noreply.github.com>
2023-01-14 11:36:06 -0500

    Update caseStudy1.sql

--- caseStudy1.sql
+++ caseStudy1.sql
@@ -13,9 +13,9 @@
 --10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
 
 /*Tables*/
--sales
--menu
--members
+--sales
+--menu
+--members
 
 /*Question 1*/
 
