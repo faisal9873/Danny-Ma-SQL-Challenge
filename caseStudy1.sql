89ae586f596c89f46ccb0ebc7afbd08e6a809d50
faisal9873 <83473510+faisal9873@users.noreply.github.com>
2023-01-14 11:34:10 -0500

    Update caseStudy1.sql

--- caseStudy1.sql
+++ caseStudy1.sql
@@ -1,12 +1,6 @@
 
 /* Case Study Questions */
 
-/*Tables*/
--sales
--menu
--members
-
-
 1. What is the total amount each customer spent at the restaurant?
 2. How many days has each customer visited the restaurant?
 3. What was the first item from the menu purchased by each customer?
@@ -17,6 +11,11 @@
 8. What is the total items and amount spent for each member before they became a member?
 9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+
+/*Tables*/
+-sales
+-menu
+-members
 
 /*Question 1*/
 
